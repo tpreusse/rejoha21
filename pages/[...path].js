ceb54efa80fc675b6fb1c22eab6d0dee3c82b668
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-05-08 15:16:20 +0200

    get basic card with links working

--- pages/[...path].js
+++ pages/[...path].js
@@ -1,13 +1,36 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { timeFormat } from "d3-time-format";
 
 import Frame from "../components/Frame";
-
 import styles from "../styles/Main.module.css";
-
 import getDoc from "../data/doc";
-
 import { ArticleIcon } from "../components/ArticleIcon/ArticleIcon";
+
+const publishDateFormat = timeFormat("%d.%m.%Y");
+
+function Card({ doc }) {
+  return (
+    <Link href={doc.url.replace('https://www.republik.ch', '')}>
+      <a className={styles.linkedArticleContainer}>
+        <div className={styles.icon}>
+          <ArticleIcon
+            textlength={doc.readingMinutes}
+            linksin={doc.internalLinkCountIn || 0}
+            linksout={doc.internalLinkCountOut || doc.internalLinkCount || 0}
+            linktype="triangles"
+          />
+        </div>
+        <div className={styles.articleData}>
+          <div className={styles.date}>
+            {publishDateFormat(new Date(doc.publishDate))}
+          </div>
+          <div className={styles.headline}>{doc.title}</div>
+        </div>
+      </a>
+    </Link>
+  );
+}
 
 export default function Detail({ doc, internalLinks, documents }) {
   const router = useRouter();
@@ -31,64 +54,34 @@ export default function Detail({ doc, internalLinks, documents }) {
         </a>
       </Link>
       <div className={styles.grid}>
-        {inLinks.map(link => {
-          const linkedDoc = documents.find(d => d.id === link.source)
+        {inLinks.map((link) => {
+          const linkedDoc = documents.find((d) => d.id === link.source);
 
-          return (
-            <div key={linkedDoc.id} className={styles.linkedArticleContainer}>
-              <div className={styles.icon}>
-                <ArticleIcon
-                  textlength={linkedDoc.readingMinutes}
-                  linksin={linkedDoc.internalLinkCountIn || 0}
-                  linksout={linkedDoc.internalLinkCountOut || 0}
-                  linktype='triangles'
-                />
-              </div>
-              <div className={styles.articleData}>
-                <div className={styles.date}>23.07.2019</div>
-                <div className={styles.headline}>
-                  {linkedDoc.title}
-                </div>
-              </div>
-            </div>
-          )
+          return <Card key={linkedDoc.id} doc={linkedDoc} />;
         })}
       </div>
-      
+
       <div className={styles.icon}>
         <ArticleIcon
           textlength={doc.readingMinutes}
           linksin={inLinks.length}
           linksout={outLinks.length}
-          linktype='triangles'
+          linktype="triangles"
+          inverted
         />
       </div>
+      {publishDateFormat(new Date(doc.publishDate))}
       <h1 className={styles.title}>{doc.title}</h1>
       <p className={styles.description}>{doc.description}</p>
-      <a href={doc.url} target='_blank'>Beitrag lesen</a>
+      <a href={doc.url} target="_blank">
+        Beitrag lesen &#x2197;
+      </a>
 
       <div className={styles.grid}>
-        {outLinks.map(link => {
-          const linkedDoc = documents.find(d => d.id === link.target)
+        {outLinks.map((link) => {
+          const linkedDoc = documents.find((d) => d.id === link.target);
 
-          return (
-            <div key={linkedDoc.id} className={styles.linkedArticleContainer}>
-              <div className={styles.icon}>
-                <ArticleIcon
-                  textlength={linkedDoc.readingMinutes}
-                  linksin={linkedDoc.internalLinkCountIn || 0}
-                  linksout={linkedDoc.internalLinkCountOut || 0}
-                  linktype='triangles'
-                />
-              </div>
-              <div className={styles.articleData}>
-                <div className={styles.date}>23.07.2019</div>
-                <div className={styles.headline}>
-                  {linkedDoc.title}
-                </div>
-              </div>
-            </div>
-          )
+          return <Card key={linkedDoc.id} doc={linkedDoc} />;
         })}
       </div>
     </Frame>
