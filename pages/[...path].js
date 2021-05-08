b84d0f0cdf9db0036e74a546d2dbd5f793214269
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-05-08 15:04:15 +0200

    render in and out links

--- pages/[...path].js
+++ pages/[...path].js
@@ -9,7 +9,7 @@ import getDoc from "../data/doc";
 
 import { ArticleIcon } from "../components/ArticleIcon/ArticleIcon";
 
-export default function Detail({ doc, internalLinks }) {
+export default function Detail({ doc, internalLinks, documents }) {
   const router = useRouter();
 
   if (router.isFallback) {
@@ -30,32 +30,67 @@ export default function Detail({ doc, internalLinks }) {
           <h2>&larr; Themen­stränge</h2>
         </a>
       </Link>
-      <div className={styles.linkedArticleContainer}>
-        <div className={styles.icon}>
-          <ArticleIcon
-            textlength={40}
-            linksin={15}
-            linksout={2}
-            linktype={"triangles"}
-          />
-        </div>
-        <div className={styles.articleData}>
-          <div className={styles.date}>23.07.2019</div>
-          <div className={styles.headline}>
-            Wenn man auf dem Weg dreimal hinfällt und dann liegen bleibt.
-          </div>
-        </div>
+      <div className={styles.grid}>
+        {inLinks.map(link => {
+          const linkedDoc = documents.find(d => d.id === link.source)
+
+          return (
+            <div key={linkedDoc.id} className={styles.linkedArticleContainer}>
+              <div className={styles.icon}>
+                <ArticleIcon
+                  textlength={linkedDoc.readingMinutes}
+                  linksin={linkedDoc.internalLinkCountIn || 0}
+                  linksout={linkedDoc.internalLinkCountOut || 0}
+                  linktype='triangles'
+                />
+              </div>
+              <div className={styles.articleData}>
+                <div className={styles.date}>23.07.2019</div>
+                <div className={styles.headline}>
+                  {linkedDoc.title}
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
+      
       <div className={styles.icon}>
         <ArticleIcon
           textlength={doc.readingMinutes}
           linksin={inLinks.length}
           linksout={outLinks.length}
-          linktype={"triangles"}
+          linktype='triangles'
         />
       </div>
       <h1 className={styles.title}>{doc.title}</h1>
       <p className={styles.description}>{doc.description}</p>
+      <a href={doc.url} target='_blank'>Beitrag lesen</a>
+
+      <div className={styles.grid}>
+        {outLinks.map(link => {
+          const linkedDoc = documents.find(d => d.id === link.target)
+
+          return (
+            <div key={linkedDoc.id} className={styles.linkedArticleContainer}>
+              <div className={styles.icon}>
+                <ArticleIcon
+                  textlength={linkedDoc.readingMinutes}
+                  linksin={linkedDoc.internalLinkCountIn || 0}
+                  linksout={linkedDoc.internalLinkCountOut || 0}
+                  linktype='triangles'
+                />
+              </div>
+              <div className={styles.articleData}>
+                <div className={styles.date}>23.07.2019</div>
+                <div className={styles.headline}>
+                  {linkedDoc.title}
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </Frame>
   );
 }
