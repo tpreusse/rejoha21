6f092a805b772c0953ab40390be7dd889abc6d07
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-05-08 13:06:44 +0200

    setup app

---
+++ data/doc.js
@@ -0,0 +1,21 @@
+import data from './2019.json'
+
+export default path => {
+  const doc = data.documents.find(d => d.url.endsWith(path))
+
+  if (!doc) {
+    return
+  }
+
+  const internalLinks = data.internalLinks.filter(link => link.source === doc.id || link.target === doc.id)
+  const externalLinks = data.externalLinks.filter(link => link.source === doc.id)
+
+  const documentIds = new Set(internalLinks.map(link => link.source).concat(internalLinks.map(link => link.target)))
+
+  return { 
+    doc,
+    documents: data.documents.filter(d => documentIds.has(d.id)),
+    internalLinks,
+    externalLinks
+  }
+}
---
+++ pages/[...path].js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+import styles from '../styles/Home.module.css'
+
+import getDoc from '../data/doc'
+
+export default function Detail({ doc }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div className={styles.container}>
+      <main className={styles.main}>
+        <p className={styles.description}>Loading...</p>
+      </main>
+    </div>
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{doc.title} – Rehoja21 Content Exploration</title>
+      </Head>
+      
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {doc.title}
+        </h1>
+        <p className={styles.description}>
+          {doc.description}
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        Ein Projekt von {' '}
+        <code className={styles.code}>Salome Ernie</code>,{' '}
+        <code className={styles.code}>Corina Mühle</code>,{' '}
+        <code className={styles.code}>Sharon Funke</code>,{' '}
+        <code className={styles.code}>Sebastian Broschinski</code> und {' '}
+        <code className={styles.code}>Thomas Preusse</code>
+      </footer>
+    </div>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const data = getDoc(params.path.join('/'))
+
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      ...data
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true
+  };
+}
